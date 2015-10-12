c4726eb69164df863795d43c6bb0b7372fd2f15d
Christopher Lowenthal <christopher.lowenthal@autodesk.com>
2015-10-12 16:41:10 -0400

    Starting on the circles interface

---
+++ packages/core/circles/server/controllers/interface/circle.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+
+  //MEAN.io loads everything in the 'models' folder automatically
+  //This provides an abstraction between the circles Mongoose model and controller
+
+  module.exports = {
+
+  };
+
+})();
